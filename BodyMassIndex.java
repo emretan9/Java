c58a0d5806b2e587e1dff22b206df22435a3cfb1
Emre Zeki TANRIKULU <81697924+emretan9@users.noreply.github.com>
2022-02-09 16:33:44 +0300

    Add files via upload

---
+++ BodyMassIndex.java
@@ -0,0 +1,25 @@
+package giris;
+import java.util.Scanner;
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        double height,weight,Index;
+
+        System.out.print("Enter your height in meter: ");
+        height=inp.nextDouble();
+        System.out.print("Enter your weight : ");
+        weight=inp.nextDouble();
+
+        Index= (weight/(height*height));
+        System.out.print("your body mass index is "+Index);
+
+
+
+
+
+
+
+
+
+    }
+}
